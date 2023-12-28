e3ebf4fdfdba18dc4631811eec9e29d8a5660d0d
ForeverVik <sirgrumple@gmail.com>
2023-12-28 15:04:12 -0500

    Converted SS to make use of doubly linked list to hold board history

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,22 +1,31 @@
 from copy import deepcopy
+from DoublyLinkedList import doublyLinkedList as DLL
 
 class SudokuSolver():
-    def __init__(self, baseBoard):
-        self.baseSudoku = deepcopy(baseBoard)
+    def __init__(self, initialBoard=[[0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0],
+                                  [0,0,0,0,0,0,0,0,0]]):
+        
+        self.DLL = DLL()
+        self.DLL.insertToEnd(initialBoard)
+        self.current = self.DLL.startNode
 
         #Mark-up the given Sudoku board by filling in every empty square with every number 1-9
-        for lineNum, line in enumerate(baseBoard):
+        for lineNum, line in enumerate(self.current.board):
             for num in range(len(line)):
                 if line[num] == 0:
                     line[num] = [1,2,3,4,5,6,7,8,9]
-                    baseBoard[lineNum] = line
-                else:
+                    self.current.board[lineNum] = line
+                elif type(line[num]) == int:
                     line[num] = [line[num]]
 
-        self.baseBoard = deepcopy(baseBoard)
-        self.prevBoard = deepcopy(baseBoard)
-        self.currentBoard = deepcopy(baseBoard)
-        self.size = len(self.baseBoard)
+        self.size = len(self.current.board)
         self.isSolved = False
         self.iterations = 0
         self.temp = []
@@ -24,7 +33,7 @@ class SudokuSolver():
 
     #Prints out the current board neatly
     def __str__(self):
-        for i in self.currentBoard:
+        for i in self.current.board:
             for j in i:
                 if len(j) > 1:
                     out = '.'
@@ -34,9 +43,9 @@ class SudokuSolver():
             print()
         return ""
 
-    #Prints out the base board neatly
-    def printBase(self):
-        for i in self.baseSudoku:
+    #Prints out the initial board neatly
+    def printInitial(self):
+        for i in self.DLL.startNode.board:
             for j in i:
                 if j == 0:
                     out = '.'
@@ -48,23 +57,23 @@ class SudokuSolver():
 
     #Returns all possible numbers for the i,j square
     def getNumsAt(self, i, j):
-        return self.currentBoard[i][j]
+        return self.current.board[i][j]
     
     def getSizeAt(self, i, j):
         return len(self.getNumsAt(i,j))
     
     def getGridAt(self, i, j):
-        return i//3 + j//3
+        return 3*(i//3) + j//3
         
     def checkIfSolved(self):
         #If the board has not changed since the last time checkIfSolved() was called, return True
         #Or if every square has only one number, return True
-        if self.temp == self.currentBoard:
+        if self.temp == self.current.board:
             self.solved = True
             return True
     
-        self.temp = deepcopy(self.currentBoard)
-        for i in self.currentBoard:
+        self.temp = deepcopy(self.current.board)
+        for i in self.current.board:
             for j in i:
                 if len(j) != 1:
                     self.solved = False
@@ -83,8 +92,8 @@ class SudokuSolver():
                 raise ValueError
         
         out = []
-        for i in range(len(self.currentBoard[row])):
-            if all(ele in self.currentBoard[row][i] for ele in num):
+        for i in range(len(self.current.board[row])):
+            if all(ele in self.current.board[row][i] for ele in num):
                 out.append(i)
         return out
 
@@ -97,7 +106,7 @@ class SudokuSolver():
                 raise ValueError
         
         out = []
-        for lineNum, line in enumerate(self.currentBoard):
+        for lineNum, line in enumerate(self.current.board):
             if all(ele in line[col] for ele in num):
                 out.append(lineNum)
         return out
@@ -147,17 +156,17 @@ class SudokuSolver():
         for i in rowRange:
             for j in colRange:
                 if onlyNum == True:
-                    if self.currentBoard[i][j] == num:
+                    if self.current.board[i][j] == num:
                         out.append([i,j])
                 else:
-                    if all(ele in self.currentBoard[i][j] for ele in num):
+                    if all(ele in self.current.board[i][j] for ele in num):
                         out.append([i,j])
         return out
     
     #Search through all the squares until a square n is found such that n has only one number in it
     #Remove the number from every square in the same row, column, and 3x3 as n
     def soloTest(self):
-        self.prevBoard = deepcopy(self.currentBoard)
+        _board = deepcopy(self.current.board)
 
         #Loop throught every square
         for i in range(self.size):
@@ -175,32 +184,35 @@ class SudokuSolver():
                     rowRemove = self.searchRow(i, [num])
                     rowRemove.remove(j)
                     for col in rowRemove:
-                        if len(self.currentBoard[i][col]) > 1 and self.currentBoard[i][col].count(num):
-                            self.currentBoard[i][col].remove(num)
+                        if len(_board[i][col]) > 1 and _board[i][col].count(num):
+                            _board[i][col].remove(num)
                     
                     #Remove the number from every other square in the column
                     colRemove = self.searchCol(j, [num])
                     colRemove.remove(i)
                     for row in colRemove:
-                        if len(self.currentBoard[row][j]) > 1 and self.currentBoard[row][j].count(num):
-                            self.currentBoard[row][j].remove(num)
+                        if len(_board[row][j]) > 1 and _board[row][j].count(num):
+                            _board[row][j].remove(num)
                     
                     #Remove the number from every other square in the 3x3 grid
                     gridRemove = self.searchGrid(i, j, [num])
                     gridRemove.remove([i,j])
                     for grid in gridRemove:
-                        if len(self.currentBoard[grid[0]][grid[1]]) > 1 and self.currentBoard[grid[0]][grid[1]].count(num):
-                            self.currentBoard[grid[0]][grid[1]].remove(num)
+                        if len(_board[grid[0]][grid[1]]) > 1 and _board[grid[0]][grid[1]].count(num):
+                            _board[grid[0]][grid[1]].remove(num)
+        
+        self.DLL.insertToEnd(_board, "solo")
+        self.current = self.current.next
     
     #Find any squares that contain a number n that shows up only once in it's row, column, or grid, and removes n from any squares in it's row, column, and grid
     def singlesTest(self):
-        self.prevBoard = deepcopy(self.currentBoard)
+        _board = deepcopy(self.current.board)
             
         for i in range(self.size):
             for j in range(self.size):
             
-                for num in self.currentBoard[i][j]:
-                    if num in self.currentBoard[i][j]:
+                for num in _board[i][j]:
+                    if num in _board[i][j]:
                         rowOccurences = self.searchRow(i, [num])
                         rowOccurences.remove(j)
                         colOccurences = self.searchCol(j, [num])
@@ -210,25 +222,28 @@ class SudokuSolver():
 
                         if len(rowOccurences) == 0 or len(colOccurences) == 0 or len(gridOccurences) == 0:
                             for k in rowOccurences:
-                                if self.currentBoard[i][k].count(num) and len(self.currentBoard[i][k]) > 1:
-                                    self.currentBoard[i][k].remove(num)
+                                if _board[i][k].count(num) and len(_board[i][k]) > 1:
+                                    _board[i][k].remove(num)
                             
                             for k in colOccurences:
-                                if self.currentBoard[k][j].count(num) and len(self.currentBoard[k][j]) > 1:
-                                    self.currentBoard[k][j].remove(num)
+                                if _board[k][j].count(num) and len(_board[k][j]) > 1:
+                                    _board[k][j].remove(num)
                             
                             for k in gridOccurences:
-                                if self.currentBoard[k[0]][k[1]].count(num) and len(self.currentBoard[k[0]][k[1]]) > 1:
-                                    self.currentBoard[k[0]][k[1]].remove(num)
+                                if _board[k[0]][k[1]].count(num) and len(_board[k[0]][k[1]]) > 1:
+                                    _board[k[0]][k[1]].remove(num)
 
-                            self.currentBoard[i][j] = [num]
+                            _board[i][j] = [num]
+
+        self.DLL.insertToEnd(_board, "singles")
+        self.current = self.current.next
                             
     def lineTest(self):
-        self.prevBoard = deepcopy(self.currentBoard)
+        _board = deepcopy(self.current.board)
             
         for i in range(self.size):
             for j in range(self.size):
-                for num in self.currentBoard[i][j]:
+                for num in _board[i][j]:
                     sameRow = True
                     sameCol = True
                     nums = self.searchGrid(i, j, [num])
@@ -241,32 +256,37 @@ class SudokuSolver():
                         
                     if sameRow:
                         rowRemove = self.searchRow(i, [num])
-                        for col in rowRemove:
-                            if self.getGridAt(i,j) != self.getGridAt(i,col):
-                                if len(self.currentBoard[i][col]) > 1 and self.currentBoard[i][col].count(num):
-                                    self.currentBoard[i][col].remove(num)
+                        if len(rowRemove) > 2:
+                            for col in rowRemove:
+                                if self.getGridAt(i,j) != self.getGridAt(i,col):
+                                    if len(_board[i][col]) > 1 and _board[i][col].count(num):
+                                        _board[i][col].remove(num)
                     
                     if sameCol:
                         colRemove = self.searchCol(j, [num])
-                        for row in colRemove:
-                            if self.getGridAt(i,j) != self.getGridAt(row,j):
-                                if len(self.currentBoard[row][j]) > 1 and self.currentBoard[row][j].count(num):
-                                    self.currentBoard[row][j].remove(num)
+                        if len(colRemove) > 2:
+                            for row in colRemove:
+                                if self.getGridAt(i,j) != self.getGridAt(row,j):
+                                    if len(_board[row][j]) > 1 and _board[row][j].count(num):
+                                        _board[row][j].remove(num)
+        
+        self.DLL.insertToEnd(_board, "line")
+        self.current = self.current.next
 
     #Finds any squares in a grid that only contain some pair of numbers (a,b). These squares can only contain a or b
     #Therefore, remove any occurences of a and b from the other squares in the grid
     #And if the two squares are in the same row, remove any occurences of a and b from the other squares in the row
     #And if the two squares are in the same column, remove any occurences of and b from the other squares in the row 
     def pairTest(self):
-        self.prevBoard = deepcopy(self.currentBoard)
+        _board = deepcopy(self.current.board)
 
         for i in range(self.size):
             for j in range(self.size):
                 toRemove = []
-                if len(self.currentBoard[i][j]) == 2:
+                if len(_board[i][j]) == 2:
                     nums = self.getNumsAt(i,j)
                     
-                    identicalGrid = self.searchGrid(i, j, self.currentBoard[i][j], True)
+                    identicalGrid = self.searchGrid(i, j, _board[i][j], True)
                     if len(identicalGrid) == 2:
                         for n in nums:
                             sameGrid = self.searchGrid(i, j, [n])
@@ -295,28 +315,31 @@ class SudokuSolver():
                                     toRemove.append([g,j,n])
 
                 for t in toRemove:
-                        if len(self.currentBoard[t[0]][t[1]]) > 1 and self.currentBoard[t[0]][t[1]].count(t[2]):
-                            self.currentBoard[t[0]][t[1]].remove(t[2])                                         
+                        if len(_board[t[0]][t[1]]) > 1 and _board[t[0]][t[1]].count(t[2]):
+                            _board[t[0]][t[1]].remove(t[2])     
+
+        self.DLL.insertToEnd(_board, "pair")
+        self.current = self.current.next                                    
                                                                    
     #If any grid contains exactly two squares with some pair of numbers (a,b), and neither a nor b occur anywhere else in the grid
     #Those squares can only contain a or b. Remove any excess numbers from those squares.
     def hiddenPairTest(self):
-        self.prevBoard = deepcopy(self.currentBoard)
+        _board = deepcopy(self.current.board)
 
         for i in range(self.size):
             for j in range(self.size):
-                if len(self.currentBoard[i][j]) > 1:
+                if len(_board[i][j]) > 1:
                     toRemove = []
-                    for k in range(len(self.currentBoard[i][j])):
-                        for l in range(k+1, len(self.currentBoard[i][j])):
-                            a = self.currentBoard[i][j][k]
-                            b = self.currentBoard[i][j][l]
+                    for k in range(len(_board[i][j])):
+                        for l in range(k+1, len(_board[i][j])):
+                            a = _board[i][j][k]
+                            b = _board[i][j][l]
 
                             gridPairs = self.searchGrid(i, j, [a,b])
 
                             if len(gridPairs) == 2 and len(self.searchGrid(i,j,[a])) == 2 and len(self.searchGrid(i,j,[b])) == 2:
                                 #Remove all excess numbers in the square
-                                temp = deepcopy(self.currentBoard[i][j])
+                                temp = deepcopy(_board[i][j])
                                 temp.remove(a)
                                 temp.remove(b)
 
@@ -331,51 +354,13 @@ class SudokuSolver():
                                     self.pairs[f"{a}{b}"] = [(i,j)]
                     
                     for t in toRemove:
-                        if len(self.currentBoard[t[0]][t[1]]) > 1 and self.currentBoard[t[0]][t[1]].count(t[2]):
-                            self.currentBoard[t[0]][t[1]].remove(t[2])
-                                
-    #Same as pairTest but for 3 numbers and squares
-    def tripletTest(self):
-        self.prevBoard = deepcopy(self.currentBoard)
+                        if len(_board[t[0]][t[1]]) > 1 and _board[t[0]][t[1]].count(t[2]):
+                            _board[t[0]][t[1]].remove(t[2])
+        
+        self.DLL.insertToEnd(_board, "hidden pair")
+        self.current = self.current.next
 
-        for i in range(self.size):
-            for j in range(self.size):
-                toRemove = []
-                if len(self.currentBoard[i][j]) == 3:
-                    nums = self.getNumsAt(i,j)
-
-                    identicalGrid = self.searchGrid(i, j, self.currentBoard[i][j], True)
-                    if len(identicalGrid) == 3:
-                        for n in nums:
-                            sameGrid = self.searchGrid(i, j, [n])
-                            for g in identicalGrid:
-                                sameGrid.remove(g)
-                        
-                        print(f"found triple {nums} at {identicalGrid}")
-
-                        #Same row
-                        if identicalGrid[0][0] == identicalGrid[1][0] == identicalGrid [2][0]:
-                            for n in nums:
-                                sameRow = self.searchRow(i, [n])
-                                for g in identicalGrid:
-                                    sameRow.remove(g[1])
-                                
-                                for g in sameRow:
-                                    toRemove.append([g[0],g[1],n])
-                        
-                        #Same col
-                        if identicalGrid[0][1] == identicalGrid[1][1] == identicalGrid[2][1]:
-                            for n in nums:
-                                sameCol = self.searchCol(j, [n])
-                                for g in identicalGrid:
-                                    sameCol.remove(g[0])
-                                
-                                for g in sameCol:
-                                    toRemove.append([g[0],g[1],n])
-
-                for t in toRemove:
-                        if len(self.currentBoard[t[0]][t[1]]) > 1 and self.currentBoard[t[0]][t[1]].count(t[2]):
-                            self.currentBoard[t[0]][t[1]].remove(t[2])    
+    #HIDDEN TRIPLE TEST
 
     #Recursively applies solving algorithms to the board until solved
     def solve(self):
@@ -386,7 +371,6 @@ class SudokuSolver():
         self.lineTest()
         self.hiddenPairTest()
         self.pairTest()
-        self.tripletTest()
 
         if self.checkIfSolved():
                 print(self.pairs)
